41dfe23bc3755147f162932762ec3b22d5ac3063
Faisal Ibrahim <faisal.ibrahim@tenpearls.com>
2021-11-29 03:28:10 +0500

    Initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = "http://api.weatherstack.com/current?access_key=2defc3ef029a9157dd39486f791d3ba7&query=" + latitude + "," + longitude + "&units=m";
+
+    request({ url:url, json: true}, (error, { body } = {}) => {
+        if (error) {
+            callback('Unable to connect', undefined);
+        } else if (body.error) {
+            callback('Unable to find forecast', undefined);
+        } else  {
+            callback(undefined, "It is currently " + body.current.temperature + " degrees");
+        }
+    });
+}
+
+module.exports = forecast;
